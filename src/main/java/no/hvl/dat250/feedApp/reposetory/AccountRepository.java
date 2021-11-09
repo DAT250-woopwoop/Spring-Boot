b7c317a24e1740ec10894aa39a704b1cda609fba
Mathias <mathias.skallerud@gmail.com>
2021-11-09 13:18:31 +0100

    Ehh ja

--- src/main/java/no/hvl/dat250/feedApp/reposetory/AccountRepository.java
+++ src/main/java/no/hvl/dat250/feedApp/reposetory/AccountRepository.java
@@ -2,7 +2,9 @@ package no.hvl.dat250.feedApp.reposetory;
 
 import no.hvl.dat250.feedApp.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.*;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Account findByUsername(String e);
 }
--- src/main/java/no/hvl/dat250/feedApp/reposetory/PollRepository.java
+++ src/main/java/no/hvl/dat250/feedApp/reposetory/PollRepository.java
@@ -2,7 +2,9 @@ package no.hvl.dat250.feedApp.reposetory;
 
 import no.hvl.dat250.feedApp.entity.Poll;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.*;
 
+@Repository
 public interface PollRepository extends JpaRepository<Poll, Long>{
 
 
