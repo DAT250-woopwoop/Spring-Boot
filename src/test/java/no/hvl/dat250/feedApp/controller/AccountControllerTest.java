c4a92c6c8123bc3c40d365a62fa4fc4e3e1bac2d
mhhundvin <mhhundvin@hotmail.com>
2021-11-11 08:57:59 +0100

    Fixed basic accountController tests. Tests if /users gives forbidden with no user and ok with a user.

--- src/test/java/no/hvl/dat250/feedApp/controller/AccountControllerTest.java
+++ src/test/java/no/hvl/dat250/feedApp/controller/AccountControllerTest.java
@@ -1,28 +1,21 @@
 package no.hvl.dat250.feedApp.controller;
 
 import no.hvl.dat250.feedApp.reposetory.PollRepository;
+import no.hvl.dat250.feedApp.service.AccountService;
+import no.hvl.dat250.feedApp.service.Impl.UserDetailsServiceImpl;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.io.UnsupportedEncodingException;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -30,28 +23,30 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @WebMvcTest(AccountController.class)
 class AccountControllerTest {
-    @Autowired
-    private WebApplicationContext context;
 
+    @Autowired
     private MockMvc mvc;
 
-    @Autowired
+    @MockBean
+    private AccountService accountService;
+    @MockBean
     private PollRepository pollRepository;
+    @MockBean
+    private UserDetailsServiceImpl userDetailsServiceImpl;
 
-    @Before
-    public void setup()  {
 
-        mvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .apply(springSecurity())
-                .build();
+    @Test
+    public void givenAuthRequestOnPrivateService_shouldBeForbidden() throws Exception {
+        mvc.perform(get("/users").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden());
     }
+
 
     @WithMockUser(value = "spring")
     @Test
-    public void givenAuthRequestOnPrivateService_shouldSucceedWith200() throws Exception{
+    public void givenAuthRequestOnPrivateService_shouldSucceedWith200() throws Exception {
         mvc.perform(get("/users").contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isForbidden());
+                .andExpect(status().isOk());
     }
 
 }
